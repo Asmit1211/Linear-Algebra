a846b25b5933b00e6599e8f2f1cb96ccd39bdced
Asmit1211 <149766685+Asmit1211@users.noreply.github.com>
2025-01-06 00:43:39 +0530

    Addition of two complex numbers.

---
+++ Practical1A.py
@@ -0,0 +1,8 @@
+# Function for addition of two complex numbers
+def add_complex(z1, z2):
+    return z1 + z2
+z1 = 3 + 2j
+z2 = 1 - 1j
+# Perform addition
+sum_z = add_complex(z1, z2)
+print(f"Addition of two complex numbers, {z1} and {z2} is {sum_z}")
